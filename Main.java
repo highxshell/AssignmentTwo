6a5e523194871e4fc286354be6d6ad7d71c1271f
highxshell <121538758+highxshell@users.noreply.github.com>
2023-01-14 08:39:13 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Person> persons = new ArrayList<>();
-        persons.add(new Employee("John", "Lennon","CTO",27045.78));
-        persons.add(new Employee("George", "Harrison","CTO",50000.00));
+        persons.add(new Employee("John", "Lennon","Musician(Legend)",27045.78));
+        persons.add(new Employee("George", "Harrison","AlsoLegend",50000.00));
         persons.add(new Student("Ringo", "Starr", 1.89));
         persons.add(new Student("Paul", "McCartney", 3.85));
         Collections.sort(persons);
